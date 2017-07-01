296639e98a5dd7ae419d67d9ccd5a1b832dadb0b
lstincoso <lsalhuatincos.o@gmail.com>
2017-06-30 23:01:42 -0500

    Añadir vistas

--- DressAppSolution/WpfApp1/MainWindow.xaml.cs
+++ DressAppSolution/WpfApp1/MainWindow.xaml.cs
@@ -10,34 +10,22 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Diagnostics;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
 
-   
-
-        private void Dress_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //Process.Start("C:\\Program Files (x86)\\Notepad++\\notepad++.exe");
-            this.Close();
-        }
-
-        private void Management_Click(object sender, RoutedEventArgs e)
-        {
-            Process.Start("C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe");
-            this.Close();
 
         }
     }
---
+++ DressAppSolution/WpfApp1/OpenningWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+   
+
+        private void Dress_Click(object sender, RoutedEventArgs e)
+        {
+            //Process.Start("C:\\Program Files (x86)\\Notepad++\\notepad++.exe");
+            this.Close();
+        }
+
+        private void Management_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe");
+            this.Close();
+
+        }
+    }
+}
