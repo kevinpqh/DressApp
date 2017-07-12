b4057106f256f0065c5d210baaa0e2de6a3049ae
kevinxpt27 <kevinxpt27@gmail.com>
2017-07-12 16:35:21 -0500

    mostrar obj

--- DressAppSolution/WpfApp1/MainWindow.xaml.cs
+++ DressAppSolution/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelixToolkit.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,20 +11,70 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
-using System.Drawing;
-using System.Windows.Resources;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Lógica de interacción para Window1.xaml
     /// </summary>
+    /// 
+
     public partial class Window1 : Window
     {
+        // private const string MODEL_PATH = @"C:\Users\kevin\Documents\DressApp\DressApp\DressAppSolution\DressApp\Resources\Models\Dresses\dress_beige.obj";
+        private const string MODEL_PATH = @"C:\Users\kevin\Documents\DressApp\DressApp\DressAppSolution\DressApp\Resources\Models\Tops\big_top_dark.obj";
+
+        Model3D device;
         public Window1()
         {
             InitializeComponent();
+            ModelVisual3D device3D = new ModelVisual3D();
+            device3D.Content = Display3d(MODEL_PATH);
+            // Add to view port
+
+            //viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+            viewPort3d.Children.Add(device3D);
+        }
+        private Model3D Display3d(string model)
+        {
+            device = null;
+            try
+            {
+                //Adding a gesture here
+                //viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+
+                //Import 3D model file
+                ModelImporter import = new ModelImporter();
+
+                //Load the 3D model file
+                device = import.Load(model);
+
+                var axis = new Vector3D(0, 0, 1);
+                var angle = 173;
+
+                var matrix = device.Transform.Value;
+                matrix.Rotate(new Quaternion(axis, angle));
+
+                device.Transform = new MatrixTransform3D(matrix);
+
+                var axis1 = new Vector3D(1, 0, 0);
+                var angle1 = -25;
+
+                var matrix1 = device.Transform.Value;
+                matrix1.Rotate(new Quaternion(axis1, angle1));
+
+                device.Transform = new MatrixTransform3D(matrix1);
+
+
+            }
+            catch (Exception e)
+            {
+                // Handle exception in case can not find the 3D model file
+                MessageBox.Show("Exception Error : " + e.StackTrace);
+            }
+            return device;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
